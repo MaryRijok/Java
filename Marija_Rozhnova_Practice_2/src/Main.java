b2a8f5596dfcb6b5ac037f5fb14220b6aa7f3faf
MaryRijok <rigkova.marina@yandex.ru>
2024-09-20 23:41:56 +0300

    Добавлена вторая практика

---
+++ Marija_Rozhnova_Practice_2/src/Main.java
@@ -0,0 +1,38 @@
+class Author {
+    private String name, email;
+    private char gender;
+
+    Author(String newName, String newEmail, char newGender) {
+        name = newName;
+        email = newEmail;
+        gender = newGender;
+    }
+
+    void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    char getGender() {
+        return gender;
+    }
+
+    String getString() {
+        // "автор - имя (пол) на адрес электронной почты "
+        return name + " (" + gender + ") at " + email;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Pasha Tehnik", "pasha@hydra.pon", 'M');
+        System.out.println(author.getString());
+    }
+}
