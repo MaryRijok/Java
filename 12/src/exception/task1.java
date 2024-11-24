622c818d8a819a21632d794e8056d44eaf31827c
MaryRijok <rigkova.marina@yandex.ru>
2024-11-25 01:58:11 +0300

    Добавлена практическая работа 12

---
+++ 12/src/exception/task1.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class Task1 {
+    public void demonstrateException() {
+        try {
+            System.out.println("Результат: " + (2 / 0));
+        } catch (ArithmeticException ex) {
+            System.out.println("Ошибка: деление на ноль невозможно");
+        }
+    }
+
+    public static void main(String[] args) {
+        Task1 demoInstance = new Task1();
+        demoInstance.demonstrateException();
+    }
+}
